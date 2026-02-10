3e0ba3b77bbe0ff0566c01aa1600f1fe28e684cb
Leximir <aleksa.stankovic@proton.me>
2026-02-10 19:12:25 +0100

    Created function show in post model

--- models/Post.php
+++ models/Post.php
@@ -62,6 +62,38 @@ class Post {
     public function show()
     {
 
+        $query =
+            "SELECT 
+            c.name AS category_name,
+            p.id,
+            p.category_id,
+            p.title,
+            p.body,
+            p.author,
+            p.created_at
+        FROM 
+            " . $this->table . " p 
+        LEFT JOIN 
+            categories c ON p.category_id = c.id
+        WHERE 
+            p.id = ?
+        LIMIT 0,1";
+
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(1, $this->id); // Bind id to query
+        $stmt->execute();
+
+        $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        $this->title = $row['title'];
+        $this->body = $row['body'];
+        $this->author = $row['author'];
+        $this->created_at = $row['created_at'];
+        $this->category_id = $row['category_id'];
+        $this->category_name = $row['category_name'];
+
+        return $stmt;
+
     }
 
 

92b7a2d1d723a45a2d780cfd4906b3df11e5d232
Leximir <aleksa.stankovic@proton.me>
2026-02-10 20:43:38 +0100

    Category show api done

--- api/category/show.php
+++ api/category/show.php
@@ -1,1 +1,33 @@
 <?php
+
+use config\Database;
+use models\Category;
+
+header("Access-Control-Allow-Origin: *"); // CORS header: dozvoljava pristup ovom endpointu sa bilo kojeg origin-a (*).
+header("Content-Type: application/json"); // Kaže klijentu da će odgovor biti JSON (application/json).
+
+include_once '../../config/Database.php';
+include_once '../../models/Category.php';
+
+// Instantiate DB & connect
+$database = new Database(); // Kreira novu instancu klase Database.
+$db = $database->connect(); // Poziva connect() da dobije PDO konekciju i čuva je u $db.
+
+// Instantiate Post object
+$category = new Category($db); // Kreira Post objekat i prosljeđuje konekciju bazi u konstruktor.
+
+// Get ID
+$category->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+// Get Post
+$category->show();
+
+// Create array
+$categoryShow = array(
+    'id' => $category->id,
+    'name' => $category->name,
+    'created_at' => $category->created_at
+);
+
+// Make JSON
+print_r(json_encode($categoryShow));
--- models/Category.php
+++ models/Category.php
@@ -27,4 +27,19 @@ class Category
         $stmt->execute();
         return $stmt;
     }
+
+    public function show()
+    {
+        $query = "SELECT * FROM " . $this->table . " WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $this->id);
+        $stmt->execute();
+
+        $row = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        $this->name = $row['name'];
+        $this->created_at = $row['created_at'];
+
+        return $stmt;
+    }
 }
